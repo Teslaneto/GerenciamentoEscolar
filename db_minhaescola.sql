ee75e304f0facab318137ac36863252b957a0d95
Geraldo <onixblackcx@gmail.com>
2024-08-05 11:55:27 -0300

    fix/banco_de_dados_minhaescola_atualizado

--- db_minhaescola.sql
+++ db_minhaescola.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/04/2024 às 20:51
+-- Tempo de geração: 05/08/2024 às 16:52
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `db_minhaescola`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_alunos`
+--
+
+CREATE TABLE `tb_alunos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `turma_id` int(11) DEFAULT NULL,
+  `escola_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_alunos`
+--
+
+INSERT INTO `tb_alunos` (`id`, `nome`, `turma_id`, `escola_id`) VALUES
+(1, 'Geraldo', NULL, 6),
+(3, 'Raimundo', NULL, 6),
+(4, 'Carlos', NULL, 7);
 
 -- --------------------------------------------------------
 
@@ -40,7 +62,14 @@ CREATE TABLE `tb_escola` (
 --
 
 INSERT INTO `tb_escola` (`endereco`, `inep`, `nome`, `status`, `id`) VALUES
-('Caxias -Ma Rua 876', 'Inep-MA', 'CMT IV', 0, 1);
+('Caxias -Ma Rua 876', 'fake001', 'CMT IV ', 0, 6),
+('Caxias -Ma Rua 900', 'fake002', 'Instituto Sol Nascente', 0, 7),
+('Caxias -Ma Rua 900', 'fake002', 'Instituto cabeção', 0, 8),
+('Caxias -Ma Rua 900', 'fake003', 'escola Vila Nova', 0, 9),
+('Caxias -Ma Rua 900', 'fake004', 'JOAO LOBO ', 0, 10),
+('Caxias -Ma Rua 900', 'fake006', 'Raimundo ', 0, 11),
+('Caxias -Ma Rua 900', 'fake006', 'Escola Vila Velha', 0, 12),
+('Caxias -Ma Rua 900', 'fake006', 'Raimundo novo', 0, 13);
 
 -- --------------------------------------------------------
 
@@ -50,15 +79,20 @@ INSERT INTO `tb_escola` (`endereco`, `inep`, `nome`, `status`, `id`) VALUES
 
 CREATE TABLE `tb_professores` (
   `nome` varchar(50) NOT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `usuarioF` varchar(40) DEFAULT NULL,
+  `senhaF` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `tb_professores`
 --
 
-INSERT INTO `tb_professores` (`nome`, `id`) VALUES
-('Geraldo', 2);
+INSERT INTO `tb_professores` (`nome`, `id`, `usuarioF`, `senhaF`) VALUES
+('Geraldo', 2, 'FGE01', 'FGE01'),
+('Shamyra', 3, 'FSH02', 'FSH02'),
+('Cecilly', 4, 'FCE03', 'FCE03'),
+('Neto', 5, 'FNE04', 'FNE04');
 
 -- --------------------------------------------------------
 
@@ -80,7 +114,6 @@ CREATE TABLE `tb_turmas` (
 --
 
 INSERT INTO `tb_turmas` (`id_escola`, `id_professores`, `nome`, `status`, `turno`, `id`) VALUES
-(1, 0, '3 ano D', 0, 'manhã', 1),
 (1, 0, '3 ano A', 0, 'manhã', 3);
 
 -- --------------------------------------------------------
@@ -97,6 +130,14 @@ CREATE TABLE `turma_professor` (
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `tb_alunos`
+--
+ALTER TABLE `tb_alunos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `turma_id` (`turma_id`),
+  ADD KEY `escola_id` (`escola_id`);
 
 --
 -- Índices de tabela `tb_escola`
@@ -129,26 +170,39 @@ ALTER TABLE `turma_professor`
 --
 
 --
+-- AUTO_INCREMENT de tabela `tb_alunos`
+--
+ALTER TABLE `tb_alunos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de tabela `tb_escola`
 --
 ALTER TABLE `tb_escola`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de tabela `tb_professores`
 --
 ALTER TABLE `tb_professores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `tb_turmas`
 --
 ALTER TABLE `tb_turmas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para tabelas despejadas
 --
+
+--
+-- Restrições para tabelas `tb_alunos`
+--
+ALTER TABLE `tb_alunos`
+  ADD CONSTRAINT `tb_alunos_ibfk_1` FOREIGN KEY (`turma_id`) REFERENCES `tb_turmas` (`id`),
+  ADD CONSTRAINT `tb_alunos_ibfk_2` FOREIGN KEY (`escola_id`) REFERENCES `tb_escola` (`id`);
 
 --
 -- Restrições para tabelas `turma_professor`
